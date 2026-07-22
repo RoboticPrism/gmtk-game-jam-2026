4c0ddde8bb57fbcb1ed86ad4308b60b36a8a0d9c
Ryan <ryan92130@gmail.com>
2026-07-22 15:45:44 -0700

    Allow lights to be removed

--- Assets/Scripts/FogOfWarLight.cs
+++ Assets/Scripts/FogOfWarLight.cs
@@ -5,4 +5,9 @@ public class FogOfWarLight : MonoBehaviour
     [SerializeField]
     [Tooltip("How large of a radius this emits light")]
     public int lightRadius;
+
+    public void OnDestroy()
+    {
+        FogOfWarManager.TriggerRemoveLightUpdate(this);
+    }
 }
