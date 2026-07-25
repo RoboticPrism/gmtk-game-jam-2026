43b3d81d95d3d14c837b496d5d3cc6e68b3157a4
steevedoss <steevedoss@gmail.com>
2026-07-24 20:18:51 -0500

    SetttingsMenu

--- Assets/Scripts/Bootstrap.cs
+++ Assets/Scripts/Bootstrap.cs
@@ -1,10 +1,23 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Bootstrap : MonoBehaviour
 {
     public static Bootstrap Instance { get; private set; }
     public AudioManager Audio { get; private set; }
+
+    public Button settingMenuButton;
+    
+    public GameObject settingsMenu;
+    
+    public Button muteButton;
+    public Sprite muteOn;
+    public Sprite muteOff;
+    private bool isMuted = false;
+    
+    public Slider volumeSlider;
+    private float currentVolume;
     
     private void Awake()
     {
@@ -17,5 +30,30 @@ public class Bootstrap : MonoBehaviour
         Instance = this;
         DontDestroyOnLoad(gameObject);
         Audio = GetComponent<AudioManager>();
+    }
+
+    void Start() {
+        settingMenuButton.onClick.AddListener(ToggleMenu);
+        muteButton.onClick.AddListener(ToggleMute);
+        volumeSlider.onValueChanged.AddListener(SetVolume);
+        settingsMenu.SetActive(false);
+        volumeSlider.value = AudioListener.volume;
+    }
+    public void ToggleMenu()
+    {
+        settingsMenu.SetActive(!settingsMenu.activeSelf);
+    }
+    public void ToggleMute()
+    {
+        isMuted = !isMuted;
+        muteButton.image.sprite = isMuted ? muteOn : muteOff;
+        AudioListener.volume = isMuted ? 0 : currentVolume;
+    }
+
+    public void SetVolume(float volume) {
+        currentVolume = volume;
+        if (!isMuted) {
+            AudioListener.volume = currentVolume;
+        }
     }
 }
